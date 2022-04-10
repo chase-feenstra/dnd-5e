1f7176e49f0fb1064e19dc543cbff45e4b98f3d5
Chase F <kunucatchu@gmail.com>
2022-04-10 14:39:45 -0500

    Cleanup and working

--- Console.Roller/Program.cs
+++ Console.Roller/Program.cs
@@ -1,34 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Roller.Domain;
 using System.CommandLine;
-using System.CommandLine.Invocation;
+using System.CommandLine.NamingConventionBinder;
 
-var command = new Command("roll", "Roll the dice!");
-
-var dieArg = new Argument<string>("die", "The dice you want to roll");
-var verbOpt = new Option(new[] { "--verbose", "-v" }, "Show the detailed roll");
-
-command.Add(dieArg);
-command.Add(verbOpt);
-
-command.SetHandler((string die, bool verbose) =>
+var command = new Command("roll", "Roll the dice!")
 {
-    var times = int.Parse(die.Split('d').First());
-    var dieNum = int.Parse(die.Split('d').Last());
+    new Argument<string>("die", "The dice you want to roll (e.g, 3d6, 1d4, 5d20)"),
+    new Option(new[] { "--verbose", "-v" }, "Show the detailed roll")
+};
+command.Handler = CommandHandler.Create(RollHandler);
 
-    var roller = new RollerService();
-    var rolls = roller.Roll(times, dieNum);
-
-    Console.WriteLine($"Rolled d{dieNum} {times} times.");
-
-    Console.WriteLine(verbose 
-        ? string.Join(Environment.NewLine, rolls) 
-        : rolls.Sum());
-
-});
-
-
-static void Handler(string die, bool verbose)
+static void RollHandler(string die, bool verbose)
 {
     var times = int.Parse(die.Split('d').First());
     var dieNum = int.Parse(die.Split('d').Last());
